992573fdd516635c52a1917679dd7c137258cb5d
RenzoA <u202113612@upc.edu.pe>
2024-11-02 03:02:53 -0500

    feat: create and update caregiver and parent are created.

---
+++ CareNestSolution/CareNest/Domain/Model/Commands/CreateParentCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace CareNestSolution.CareNest.Domain.Model.Commands;
+
+public class CreateParentCommand
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Document { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    
+    public CreateParentCommand(string name, string surname, string email, string document, string phoneNumber, string password)
+    {
+        Name = name;
+        Surname = surname;
+        Email = email;
+        Document = document;
+        PhoneNumber = phoneNumber;
+        Password = password;
+    }
+}
