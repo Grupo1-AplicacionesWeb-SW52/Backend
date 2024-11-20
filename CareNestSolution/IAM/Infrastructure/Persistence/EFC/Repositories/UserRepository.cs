ed2e7fb14641f517f679f571c139ec25ac2d705a
RenzoA <u202113612@upc.edu.pe>
2024-11-20 13:37:25 -0500

    feat(iam): User repository are created.

--- CareNestSolution/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
+++ CareNestSolution/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -1,6 +1,34 @@
-﻿namespace CareNestSolution.IAM.Infrastructure.Persistence.EFC.Repositories;
+﻿using CareNestSolution.IAM.Domain.Model.Aggregates;
+using CareNestSolution.IAM.Domain.Repositories;
+using CareNestSolution.Shared.Infrastructure.Persistence.EFC.Configuration;
+using CareNestSolution.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
 
-public class UserRepository
+namespace CareNestSolution.IAM.Infrastructure.Persistence.EFC.Repositories;
+
+public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository
 {
-    
+    /**
+     * <summary>
+     *     Find a user by username
+     * </summary>
+     * <param name="username">The username to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> FindByUsernameAsync(string username)
+    {
+        return await Context.Set<User>().FirstOrDefaultAsync(user => user.Username.Equals(username));
+    }
+
+    /**
+     * <summary>
+     *     Check if a user exists by username
+     * </summary>
+     * <param name="username">The username to search</param>
+     * <returns>True if the user exists, false otherwise</returns>
+     */
+    public bool ExistsByUsername(string username)
+    {
+        return Context.Set<User>().Any(user => user.Username.Equals(username));
+    }
 }
