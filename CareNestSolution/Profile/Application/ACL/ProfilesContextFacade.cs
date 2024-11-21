859f1fc462310f88a702ed4c96dba377161317b5
RenzoA <u202113612@upc.edu.pe>
2024-11-21 03:26:25 -0500

    feat(profile): ProfilesContextFacade are implemented

--- CareNestSolution/Profile/Application/ACL/ProfilesContextFacade.cs
+++ CareNestSolution/Profile/Application/ACL/ProfilesContextFacade.cs
@@ -1,6 +1,51 @@
-﻿namespace CareNestSolution.Profile.Application.ACL;
+﻿using CareNestSolution.Profile.Domain.Model.Commands;
+using CareNestSolution.Profile.Domain.Model.Queries;
+using CareNestSolution.Profile.Domain.Model.ValueObjects;
+using CareNestSolution.Profile.Domain.Services;
+using CareNestSolution.Profile.Interfaces.ACL;
 
-public class ProfilesContextFacade
+namespace CareNestSolution.Profile.Application.ACL;
+
+public class ProfilesContextFacade(
+    IProfileCommandService profileCommandService,
+    IProfileQueryService profileQueryService) : IProfilesContextFacade
 {
-    
+    public async Task<int> CreateProfile(
+        string fullName,
+        string email,
+        string phone,
+        string document,
+        string password,
+        string profileImg,
+        string address,
+        string district)
+    {
+        var createProfileCommand = new CreateProfileCommand(
+            fullName,
+            email,
+            phone,
+            document,
+            password,
+            profileImg,
+            address,
+            district);
+        var profile = await profileCommandService.Handle(createProfileCommand);
+        return profile?.Id ?? 0;
+    }
+
+    public async Task<int> FetchProfileIdByEmail(string email)
+    {
+        var getProfileByEmailQuery = new GetProfileByEmailQuery(new EmailAddress(email));
+        var profile = await profileQueryService.Handle(getProfileByEmailQuery);
+        return profile?.Id ?? 0;
+    }
 }
+
+/* string FullName,
+    string Email,
+    string Phone,
+    string Document,
+    string Password,
+    string ProfileImg,
+    string Address,
+    string District*/
---
+++ CareNestSolution/Profile/Interfaces/ACL/IProfilesContextFacade.cs
@@ -0,0 +1,18 @@
+﻿namespace CareNestSolution.Profile.Interfaces.ACL;
+
+public interface IProfilesContextFacade
+{
+    Task<int> CreateProfile(
+        string fullName,
+        string email,
+        string phone,
+        string document,
+        string password,
+        string profileImg,
+        string address,
+        string district);
+    
+
+
+    Task<int> FetchProfileIdByEmail(string email);
+}
